dc1bf9b5bf225b1ab8cf876102ce7c14c7db32d3
丁俊杰 <10725046+JunJD@user.noreply.gitee.com>
2024-10-18 08:26:50 +0800

    chore: 1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,8 +8,6 @@ mod config;
 mod tools;
 
 fn main() {
-    tools::utils::init_logging_config();
-    
     tauri::Builder::default()
         .run(tauri::generate_context!())
     .expect("error while running tauri application");
